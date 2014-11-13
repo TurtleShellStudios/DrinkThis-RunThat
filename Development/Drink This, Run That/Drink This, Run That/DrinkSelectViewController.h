9b5fddfcd1ebe983a298a88d7c0ee6f2f8427ed8
Michael Hartung <hartungm@mail.gvsu.edu>
2014-11-13 10:52:04 -0500

    Made table view do stuff

---
+++ Development/Drink This, Run That/Drink This, Run That/DrinkSelectViewController.h
@@ -0,0 +1,13 @@
+//
+//  DrinkSelectViewController.h
+//  Drink This, Run That
+//
+//  Created by Michael Hartung on 11/13/14.
+//  Copyright (c) 2014 hartung-michael. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+@interface DrinkSelectViewController : PFQueryTableViewController
+
+@end
---
+++ Development/Drink This, Run That/Drink This, Run That/DrinkSelectViewController.m
@@ -0,0 +1,67 @@
+//
+//  DrinkSelectViewController.m
+//  Drink This, Run That
+//
+//  Created by Michael Hartung on 11/13/14.
+//  Copyright (c) 2014 hartung-michael. All rights reserved.
+//
+
+#import "DrinkSelectViewController.h"
+
+@interface DrinkSelectViewController ()
+
+@end
+
+@implementation DrinkSelectViewController
+
+-(void)viewDidLoad
+{
+	self.parseClassName = @"Drinks";
+	self.pullToRefreshEnabled = NO;
+	self.paginationEnabled = YES;
+	self.objectsPerPage = 20;
+	[super viewDidLoad];
+}
+
+-(PFQuery *)queryForTable
+{
+	PFQuery *query = [PFQuery queryWithClassName:self.parseClassName];
+	if(self.objects.count == 0)
+	{
+		query.cachePolicy = kPFCachePolicyCacheThenNetwork;
+	}
+	[query orderByAscending:@"title"];
+	return query;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath object:(PFObject *)object
+{
+	static NSString *cellIdentifier = @"Cell";
+	
+	PFTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+	if (!cell)
+	{
+		cell = [[PFTableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellIdentifier];
+	}
+	cell.textLabel.text = object[@"title"];
+	//can't figure out why this isn't working...
+	//cell.detailTextLabel.text = object[@"calories"];
+	return cell;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
